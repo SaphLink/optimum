363d6761413371a83c3a45e444aedf205d1cc159
Usama Safdar Khan <usamasafdarkhan@gmail.com>
2024-05-29 21:01:21 +0500

    responisve fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -193,7 +193,7 @@ export default function Home() {
       }}
     >
       <Popup />
-      <BoxSection hero>
+      <BoxSection hero className="pt-[110px]">
         <BoxSplitSection>
           <Box>
             <Logo src="../images/homepage/Optimum Laser Brown White.png" />
