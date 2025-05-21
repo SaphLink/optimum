3025ee5c0ed365639856c0e2e7306f5ad95b5133
Oybek Artikov <artikov.dev@gmail.com>
2025-05-21 21:55:33 +0500

    Popup bug fix & update

--- src/app/popup.tsx
+++ src/app/popup.tsx
@@ -1,7 +1,6 @@
 import { modal } from "@/components/modal";
 import React, { useEffect, useRef, useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
-import priceTable from "../../public/images/cropped-images/popup-deal.png";
 import Image from "next/image";
 
 const popupTimeout = 0;
@@ -14,31 +13,37 @@ const Popup = () => {
 		setTimeout(async () => {
 			await modal(({ show, proceed }: any) => {
 				const [showPopupState, setShowPopup] = useState(false);
+
 				useEffect(() => {
-					setTimeout(() => {
+					const timer = setTimeout(() => {
 						setShowPopup(show);
-					}, 10000);
+					}, 8000);
+					return () => clearTimeout(timer);
 				}, [show]);
 
 				return (
 					<>
 						<div
-							className={`fixed duration-300 top-0 left-0 w-full h-full z-[500] ${
-								showPopupState ? "bg-[rgba(0,0,0,0.3)] backdrop-blur-[3px]" : ""
-							} flex items-center justify-center`}
+							className={`fixed inset-0 z-[500] flex items-center justify-center pointer-events-none`}
 						>
 							<div
-								className={`relative rounded-[2rem] bg-white border-[3px] border-[#35281e] md:min-w-[500px] w-[90%] md:max-w-[768px] duration-300 ${
+								className={`absolute inset-0 transition duration-300 ${
 									showPopupState
-										? "translate-y-[0] opacity-1"
-										: "translate-y-[10px] opacity-0"
+										? "bg-[rgba(0,0,0,0.3)] backdrop-blur-[3px] pointer-events-auto"
+										: "pointer-events-none"
+								}`}
+								onClick={proceed}
+							></div>
+							<div
+								className={`relative rounded-[2rem] bg-white border-[3px] border-[#35281e] md:min-w-[500px] w-[90%] md:max-w-[768px] transition-all duration-300 z-[10] ${
+									showPopupState
+										? "translate-y-0 opacity-100 pointer-events-auto"
+										: "translate-y-[10px] opacity-0 pointer-events-none"
 								}`}
 							>
 								<div
-									className="cursor-pointer absolute top-[0] translate-x-[50%] translate-y-[-50%] flex items-center justify-center right-[0]  h-[50px] w-[50px] bg-[#f5f6f6] border-[3px] border-[#5b4d42] rounded-[50%] z-[10]"
-									onClick={() => {
-										proceed();
-									}}
+									className="cursor-pointer absolute top-0 right-0 translate-x-[50%] translate-y-[-50%] flex items-center justify-center h-[50px] w-[50px] bg-[#f5f6f6] border-[3px] border-[#5b4d42] rounded-full z-10"
+									onClick={proceed}
 								>
 									<IoCloseSharp className="text-[2rem] text-[#c5ba9d]" />
 								</div>
@@ -50,7 +55,7 @@ const Popup = () => {
 											alt="popup"
 											width={500}
 											height={500}
-											className="w-full h-full object-cover "
+											className="w-full h-full object-cover"
 										/>
 									</div>
 
@@ -62,14 +67,14 @@ const Popup = () => {
 											height={200}
 											className="sm:w-[200px] sm:h-[100px] w-[100px] h-[50px] object-contain"
 										/>
-										<h1 className="text-3xl m-4">FREE Consultation</h1>
-										<h2 className="text-xl pb-8">
+										<h1 className="text-3xl my-4">FREE Consultation</h1>
+										<h2 className="text-xl pb-4 lg:pb-8">
 											+ 20% Off Your First Package
 										</h2>
 										<form
 											action={`https://formsubmit.co/${process.env.NEXT_PUBLIC_EMAIL}`}
 											method="POST"
-											className="flex w-full  flex-col gap-x-3 items-center p-4"
+											className="flex w-full flex-col gap-x-3 items-center p-4"
 											style={{ fontFamily: "Raleway" }}
 										>
 											<input
@@ -79,7 +84,7 @@ const Popup = () => {
 											/>
 											<input
 												name="name"
-												className="w-full px-4 py-2  border-2 border-[#35281e] rounded-[4px] mb-4"
+												className="w-full px-4 py-2 border-2 border-[#35281e] rounded-[4px] mb-4"
 												placeholder="YOUR NAME"
 											/>
 											<input
@@ -94,7 +99,7 @@ const Popup = () => {
 											/>
 											<button
 												type="submit"
-												className="font-bold sm:text-sm cursor-pointer w-full px-4 py-2  border-2 border-[#35281e] mb-4 text-[#35281e] bg-[#f8eedf] rounded-[4px] hover:bg-[#35281e] hover:text-[#f8eedf] transition duration-300"
+												className="font-bold sm:text-sm cursor-pointer w-full px-4 py-2 border-2 border-[#35281e] mb-4 text-[#35281e] bg-[#f8eedf] rounded-[4px] hover:bg-[#35281e] hover:text-[#f8eedf] transition duration-300"
 											>
 												Get My Custom Quote!
 											</button>
@@ -113,7 +118,7 @@ const Popup = () => {
 		!shown.current && showPopup();
 	}, []);
 
-	return <div></div>;
+	return null;
 };
 
 export default Popup;
